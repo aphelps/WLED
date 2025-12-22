0b2db93665bf2c5bb240e931f0900e1c6fa73d82
Adam Phelps <366518+aphelps@users.noreply.github.com>
2025-12-21 22:09:53 -0500

    Set to esp32dev, tested OTA, initial effect in usermod

--- wled00/usermod.cpp
+++ wled00/usermod.cpp
@@ -10,9 +10,32 @@
 
 //Use userVar0 and userVar1 (API calls &U0=,&U1=, uint16_t)
 
+/*
+ * AMP's initial test mode
+ */
+uint16_t mode_amp_test(void) {
+  uint32_t cycleTime = 1000 + (255 - SEGMENT.speed)*100;
+  uint32_t it = strip.now / cycleTime;
+  if (it != SEGENV.step) {
+    SEGENV.aux0 = (SEGENV.aux0 +1) % 256;
+    SEGENV.step = it;
+  }
+  SEGMENT.fill(SEGMENT.color_from_palette(SEGENV.aux0,
+                                     true,
+                                        (strip.paletteBlend == 1 || strip.paletteBlend == 3),
+                                        0));
+
+  return FRAMETIME;
+}
+static const char _data_FX_MODE_AMP_TEST[] PROGMEM = "AMP Test Usermod@!;!,!;!;01";
+
+
 //gets called once at boot. Do all initialization that doesn't depend on network here
 void userSetup()
 {
+  WS2812FX *fx = WS2812FX::getInstance();
+
+  fx->addEffect(255, &mode_amp_test, _data_FX_MODE_AMP_TEST);
 
 }
 
